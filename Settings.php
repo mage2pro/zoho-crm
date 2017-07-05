b116076178fb6339624ceb792fc65ac64447eca2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-05 11:47:06 +0300

    no message

---
+++ Settings.php
@@ -0,0 +1,5 @@
+<?php
+namespace Dfe\ZohoCRM;
+// 2017-07-05
+/** @method static Settings s() */
+final class Settings extends \Df\Zoho\Settings {}
--- T/Basic.php
+++ T/Basic.php
@@ -1,7 +1,11 @@
 <?php
 namespace Dfe\ZohoCRM\T;
+use Dfe\ZohoCRM\Settings as S;
 // 2017-07-05
 final class Basic extends TestCase {
 	/** @test 2017-07-05 */
 	function t00() {}
+
+	/** @test 2017-07-05 */
+	function t01() {echo S::s()->token();}
 }
