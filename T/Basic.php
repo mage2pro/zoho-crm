5be6bf9702858fcabb95a6188cfa2f73620c8fe5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-05 21:53:01 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -6,6 +6,6 @@ final class Basic extends TestCase {
 	/** @test 2017-07-05 */
 	function t00() {}
 
-	/** @test 2017-07-05 */
-	function t01() {echo S::s()->token();}
+	/** 2017-07-05 */
+	function t01_token() {echo S::s()->token();}
 }
