a23daf4f1eec7587239c328c7e614115d3444682
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 03:53:14 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Basic.php
+++ Test/Basic.php
@@ -5,11 +5,11 @@ use Dfe\ZohoCRM\Settings as S;
 # 2017-07-05
 final class Basic extends TestCase {
 	/** 2017-07-05 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-07-05 */
-	function t01_token() {print_r(S::s()->token());}
+	function t01_token():void {print_r(S::s()->token());}
 
 	/** 2017-07-08 @test */
-	function t02_getModules() {print_r(df_json_encode(F::s()->getModules()));}
+	function t02_getModules():void {print_r(df_json_encode(F::s()->getModules()));}
 }
