8611a3aa543565d5c8f1b466a9d7673c1b721dfb
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 03:49:40 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings.php
+++ Settings.php
@@ -6,7 +6,6 @@ final class Settings extends \Df\Zoho\Settings {
 	/**
 	 * 2017-07-08
 	 * @used-by \Dfe\ZohoCRM\API\Client::urlBase()
-	 * @return string
 	 */
-	function domain() {return $this->v(null, null, $this->v('domain_custom'));}
+	function domain():string {return $this->v(null, null, $this->v('domain_custom'));}
 }
